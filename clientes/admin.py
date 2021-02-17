ee270eed69191fd26c64b4262e7f9a40f64c5e59
LincolnBinda <lincoln.binda.silva@gmail.com>
2021-02-17 19:27:37 -0300

    De hoje

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Cliente, Telefone, CPF
 
+admin.site.register(Cliente)
+admin.site.register(Telefone)
+admin.site.register(CPF)
 # Register your models here.
--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class CPF(models.Model):
+    numero = models.CharField(max_length=20)
+    data_exp = models.DateTimeField(auto_now=False)
+
+    def __str__(self):
+        return self.numero
+
+
+class Cliente(models.Model):
+    name = models.CharField(max_length=70, blank=False, null=False)
+    endereco = models.CharField(max_length=500, blank=True, null=True)
+    salario = models.DecimalField(max_digits=10, decimal_places=2)
+    idaide = models.IntegerField()
+    email = models.EmailField()
+    cpf = models.OneToOneField(CPF, blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ('Nome: '+self.name)
+
+
+class Telefone(models.Model):
+    numero = models.CharField(max_length=20)
+    descricao = models.CharField(max_length=200)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.descricao + '-' + self.numero
